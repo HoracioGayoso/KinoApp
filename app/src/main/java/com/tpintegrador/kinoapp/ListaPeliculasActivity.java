2e8a105087578aa0785caf862fb38af901d0d025
Joaco <joaco.fernandez.2212@gmail.com>
2023-01-16 15:59:55 -0300

    Added login layout and fixed previous bugs. Layout logic pending.

--- app/src/main/java/com/tpintegrador/kinoapp/ListaPeliculasActivity.java
+++ app/src/main/java/com/tpintegrador/kinoapp/ListaPeliculasActivity.java
@@ -23,17 +23,17 @@ public class ListaPeliculasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         peliculas.add(new Pelicula(1, "Avengers", "2012", R.mipmap.avengers));
-        peliculas.add(new Pelicula(1, "Avatar", "2009", R.mipmap.avatar_foreground));
-        peliculas.add(new Pelicula(1, "Harry Potter", "2001", R.mipmap.harry_foreground));
-        peliculas.add(new Pelicula(1, "Avengers", "2012", R.mipmap.avengers));
-        peliculas.add(new Pelicula(1, "Avatar", "2009", R.mipmap.avatar_foreground));
-        peliculas.add(new Pelicula(1, "Harry Potter", "2001", R.mipmap.harry_foreground));
-        peliculas.add(new Pelicula(1, "Avengers", "2012", R.mipmap.avengers));
-        peliculas.add(new Pelicula(1, "Avatar", "2009", R.mipmap.avatar_foreground));
-        peliculas.add(new Pelicula(1, "Alicia en el pais de las maravillas", "2001", R.mipmap.harry_foreground));
-        peliculas.add(new Pelicula(1, "Avengers", "2012", R.mipmap.avengers));
-        peliculas.add(new Pelicula(1, "Avatar", "2009", R.mipmap.avatar_foreground));
-        peliculas.add(new Pelicula(1, "Harry Potter", "2001", R.mipmap.harry_foreground));
+        peliculas.add(new Pelicula(2, "Avatar", "2009", R.mipmap.avatar_foreground));
+        peliculas.add(new Pelicula(3, "Harry Potter", "2001", R.mipmap.harry_foreground));
+        peliculas.add(new Pelicula(4, "Avengers", "2012", R.mipmap.avengers));
+        peliculas.add(new Pelicula(5, "Avatar", "2009", R.mipmap.avatar_foreground));
+        peliculas.add(new Pelicula(6, "Harry Potter", "2001", R.mipmap.harry_foreground));
+        peliculas.add(new Pelicula(7, "Avengers", "2012", R.mipmap.avengers));
+        peliculas.add(new Pelicula(8, "Avatar", "2009", R.mipmap.avatar_foreground));
+        peliculas.add(new Pelicula(9, "Alicia en el pais de las maravillas", "2001", R.mipmap.harry_foreground));
+        peliculas.add(new Pelicula(10, "Avengers", "2012", R.mipmap.avengers));
+        peliculas.add(new Pelicula(11, "Avatar", "2009", R.mipmap.avatar_foreground));
+        peliculas.add(new Pelicula(12, "Harry Potter", "2001", R.mipmap.harry_foreground));
         mBinding = ListaPelisBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
         System.out.println(peliculas.size() + " peliculas");
